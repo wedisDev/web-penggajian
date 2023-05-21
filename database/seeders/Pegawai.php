f2e3eb32c0e6ebe311359a996f2306bd21ca90a9
Rrens <yusufrendi313@gmail.com>
2023-05-22 01:08:41 +0700

    a

--- database/seeders/Pegawai.php
+++ database/seeders/Pegawai.php
@@ -15,18 +15,18 @@ class Pegawai extends Seeder
      */
     public function run()
     {
-        DB::table('pegawais')->insert([
-            [
-                'nama_pegawai' => 'Rendy',
-                'id_jabatan' => 1,
-                'id_cabang' => 1,
-                'jenis_kelamin' => 'Laki-laki',
-                'alamat' => 'Surabaya',
-                'status' => 'menikah',
-                'tahun_masuk' => Carbon::now(),
-                'jumlah_anak' => 3,
-                'created_at' => Carbon::now()
-            ]
-        ]);
+        // DB::table('pegawais')->insert([
+        //     [
+        //         'nama_pegawai' => 'Rendy',
+        //         'id_jabatan' => 1,
+        //         'id_cabang' => 1,
+        //         'jenis_kelamin' => 'Laki-laki',
+        //         'alamat' => 'Surabaya',
+        //         'status' => 'menikah',
+        //         'tahun_masuk' => Carbon::now(),
+        //         'jumlah_anak' => 3,
+        //         'created_at' => Carbon::now()
+        //     ]
+        // ]);
     }
 }
